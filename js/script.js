bf0016f2a52216c6c228d8cf4574611d7530ff31
rafahk2o <49276128+rafahk2o@users.noreply.github.com>
2022-06-30 10:29:44 -0300

    casa

--- js/script.js
+++ js/script.js
@@ -21,11 +21,21 @@ function folhaPagamento() {
     if ((salario.value >= 1212.01) && (salario.value <= 2427.35)) {
         let liquido = (salario.value - (salario.value * 0.30));
         relatorio.innerHTML = `<p> O funcionário ${nome.value} que ocupa o cargo de ${cargo.value}
-        no departamento${departamento.value} com o salário bruto de ${salario.value} ficará com um salário liquido de${liquido} </p>`
+        no departamento ${departamento.value} com o salário bruto de ${salario.value} ficará com um salário liquido de ${liquido} </p>`
 
-    } else if((salario.value >= 2427.36) && (salario.value <= 3641.03)){
-        let liquido = (salario.value - (salario.value * 0.30));
+    } else if ((salario.value >= 2427.36) && (salario.value <= 3641.03)) {
+        let liquido = (salario.value - (salario.value * 0.45));
         relatorio.innerHTML = `<p> O funcionário ${nome.value} que ocupa o cargo de ${cargo.value}
-        no departamento${departamento.value} com o salário bruto de ${salario.value} ficará com um salário liquido de${liquido} </p>
+        no departamento ${departamento.value} com o salário bruto de ${salario.value} ficará com um salário liquido de ${liquido} </p>`
+    
+    } else if ((salario.value >= 3641.04) && (salario.value <= 7087.22)) {
+        let liquido = (salario.value - (salario.value * 0.475));
+        relatorio.innerHTML = `<p> O funcionário ${nome.value} que ocupa o cargo de ${cargo.value}
+        no departamento ${departamento.value} com o salário bruto de ${salario.value} ficará com um salário liquido de ${liquido} </p>`
+    
+    } else if (salario.value >= 7087.23) {
+        let liquido = (salario.value - (salario.value * 0.275));
+        relatorio.innerHTML = `<p> O funcionário ${nome.value} que ocupa o cargo de ${cargo.value}
+        no departamento ${departamento.value} com o salário bruto de ${salario.value} ficará com um salário liquido de ${liquido - 466} </p>`
     }
 }
